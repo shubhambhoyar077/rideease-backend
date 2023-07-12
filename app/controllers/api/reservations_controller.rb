1aa592792b2493d92256a6b152d7ce84086d3a9d
shubham <shubham.bhoyar077@gmail.com>
2023-07-12 20:50:34 +0530

    Fix linters

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -2,6 +2,7 @@ class Api::ReservationsController < ApplicationController
   def show
     # Show reservation on reservation page
   end
+
   def create
     # create reservation using post method
   end
