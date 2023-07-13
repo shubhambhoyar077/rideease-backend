d9e1e7b1898e6e38220dbc9ad350095fea74d3e1
unknown <sohailshahidi1435@gmail.com>
2023-07-13 16:37:05 +0430

    Update Services Controller

--- app/controllers/api/services_controller.rb
+++ app/controllers/api/services_controller.rb
@@ -1,14 +1,51 @@
 class Api::ServicesController < ApplicationController
+  before_action :set_service, only: %i[ show update destroy ]
+
+  # GET /services
   def index
-    # For cars list: main page
-    render json: Service.all
+    @services = Service.all
+
+    render json: @services
   end
 
+  # GET /services/1
   def show
-    # For car details page
+    render json: @service
   end
 
+  # POST /services
   def create
-    # For create Cars using post method.
+    @service = Service.new(service_params)
+
+    if @service.save
+      render json: @service, status: :created, location: @service
+    else
+      render json: @service.errors, status: :unprocessable_entity
+    end
   end
+
+  # PATCH/PUT /services/1
+  def update
+    if @service.update(service_params)
+      render json: @service
+    else
+      render json: @service.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /services/1
+  def destroy
+    @service.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_service
+      @service = Service.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def service_params
+      params.fetch(:service, {})
+    end
 end
