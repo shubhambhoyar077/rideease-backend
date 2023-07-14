4e6688dbade848b3aacdc4c9be088ba25d6b4466
shubham <shubham.bhoyar077@gmail.com>
2023-07-14 17:16:17 +0530

    Fix registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    registration: 'users/registrations',
+    registrations: 'users/registrations',
     confirmations: 'users/confirmations'
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
