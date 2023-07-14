6a89a5ff424fa8459d122637808a499081544565
shubham <shubham.bhoyar077@gmail.com>
2023-07-14 09:52:12 +0530

    Add user conformation config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
-  devise :database_authenticatable, :registerable, :validatable,
+  devise :database_authenticatable, :registerable, :validatable, :confirmable,
   :jwt_authenticatable, jwt_revocation_strategy: self
   has_many :reservations
   has_many :services, through: :reservations
