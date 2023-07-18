f0c3e1465b154b4f21b38616b82598b7a7f36bd2
shubham <shubham.bhoyar077@gmail.com>
2023-07-18 13:04:05 +0530

    Create current_user using jwt token

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -1,5 +1,7 @@
 class Api::ReservationsController < ApplicationController
+  before_action :authenticate_user!
   def index
+
     @reservations = current_user.reservations.includes(:service)
     @reservation_data = @reservations.map do |reservation|
       { reservation:, service: reservation.service }
@@ -19,5 +21,13 @@ class Api::ReservationsController < ApplicationController
     head :no_content
   rescue ActiveRecord::RecordNotFound
     head :not_found
+  end
+
+  private
+
+  def current_user
+    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], 
+    Rails.application.credentials.fetch(:secret_key_base)).first
+    current_user = User.find(jwt_payload['sub'])
   end
 end
