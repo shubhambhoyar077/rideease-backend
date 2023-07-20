4a7160d5e2ee7402309187d3b40e4180791514fb
shubham <shubham.bhoyar077@gmail.com>
2023-07-20 20:31:02 +0530

    Fix auth issue and add test for conformation mail

--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -1,6 +1,85 @@
 require 'swagger_helper'
 
 RSpec.describe Users::SessionsController, type: :request do
+  path '/users' do
+    post 'User Sign Up' do
+      tags 'Users'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          user: {
+            type: :object,
+            properties: {
+              name: {type: :string},
+              email: { type: :string },
+              password: { type: :string }
+            },
+            required: %w[email password]
+          }
+        },
+        required: %w[user]
+      }
+      
+
+      response '200', 'SignUp successfully please check your email' do
+        schema type: :object,
+               properties: {
+                 status: {
+                   type: :object,
+                   properties: {
+                     code: { type: :integer },
+                     message: { type: :string },
+                     data: {
+                       type: :object,
+                       properties: {
+                         id: { type: :integer },
+                         name: { type: :string },
+                         email: { type: :string },
+                        },
+                        required: %w[id name email]
+                      }
+                    },
+                    required: %w[code message data]
+                  },
+                    },
+               required: %w[status data]
+
+        run_test! do
+      
+          post '/users', params: { user: { name: "test", email: 'test@example.com', password: 'password' } }
+
+
+          expect(response).to have_http_status(:ok)
+
+          json_response = JSON.parse(response.body)
+          expect(json_response).to include('status')
+          expect(json_response['status']).to include('code', 'message')
+          expect(json_response['status']['data']).to include('id', 'name', 'email')
+        end
+      end
+
+      response '401', "Couldn't find an active session" do
+        schema type: :object,
+               properties: {
+                 status: { type: :integer },
+                 message: { type: :string }
+               },
+               required: %w[status message]
+
+        run_test! do
+          post '/users', params: { user: { name:"invalid", email: 'invalid@example.com', password: 'invalidpassword' } }
+
+          expect(response).to have_http_status(:unauthorized)
+          json_response = JSON.parse(response.body)
+          expect(json_response).to include('status', 'message')
+        end
+      end
+    end
+  end
+
+
   path '/users/sign_in' do
     post 'User Sign In' do
       tags 'Users'
@@ -9,12 +88,18 @@ RSpec.describe Users::SessionsController, type: :request do
       parameter name: :user, in: :body, schema: {
         type: :object,
         properties: {
-          user:{
-          email: { type: :string },
-          password: { type: :string }
-        }},
-        required: %w[email password]
+          user: {
+            type: :object,
+            properties: {
+              email: { type: :string },
+              password: { type: :string }
+            },
+            required: %w[email password]
+          }
+        },
+        required: %w[user]
       }
+      
 
       response '200', 'Logged in successfully' do
         schema type: :object,
@@ -23,20 +108,20 @@ RSpec.describe Users::SessionsController, type: :request do
                    type: :object,
                    properties: {
                      code: { type: :integer },
-                     message: { type: :string }
-                   },
-                   required: %w[code message]
-                 },
-                 data: {
-                   type: :object,
-                   properties: {
-                     id: { type: :integer },
-                     name: { type: :string },
-                     email: { type: :string },
-                   },
-                   required: %w[id name email]
-                 }
-               },
+                     message: { type: :string },
+                     data: {
+                       type: :object,
+                       properties: {
+                         id: { type: :integer },
+                         name: { type: :string },
+                         email: { type: :string },
+                        },
+                        required: %w[id name email]
+                      }
+                    },
+                    required: %w[code message data]
+                  },
+                    },
                required: %w[status data]
 
         run_test! do
@@ -64,13 +149,9 @@ RSpec.describe Users::SessionsController, type: :request do
                required: %w[status message]
 
         run_test! do
-          # Make a request to sign in without valid credentials
           post '/users/sign_in', params: { user: { email: 'invalid@example.com', password: 'invalidpassword' } }
 
-          # Assert the response status code
           expect(response).to have_http_status(:unauthorized)
-
-          # Assert the response body against the defined schema
           json_response = JSON.parse(response.body)
           expect(json_response).to include('status', 'message')
         end
@@ -78,7 +159,6 @@ RSpec.describe Users::SessionsController, type: :request do
     end
   end
 
-  # let(:Authorization) { "bearer_auth #{access_token}" }
     path '/users/sign_out' do
       delete 'User Sign Out' do
         tags 'Users'
@@ -94,21 +174,81 @@ RSpec.describe Users::SessionsController, type: :request do
                  required: %w[status message]
   
           run_test! do
-            # Create a sample user for testing
             @user = User.create!(full_name: 'John Doe', email: 'john@example.com', password: 'password', role: 'user')
             @user.config
   
-            # Authenticate the user and get the bearer token
             post '/users/sign_in', params: { user: { email: 'john@example.com', password: 'password' } }
             auth_token = response.headers['Authorization']
-  
-            # Make a request to sign out with the bearer token in the Authorization header
             delete '/users/sign_out', headers: { 'Authorization' => auth_token }
   
-            # Assert the response status code
+            expect(response).to have_http_status(:ok)
+            json_response = JSON.parse(response.body)
+            expect(json_response).to include('status', 'message')
+          end
+        end
+      end
+    end
+
+    path '/users/confirmation' do
+      post 'Send conformation email again' do
+        tags 'Users'
+        consumes 'application/json'
+        produces 'application/json'
+        parameter name: :user, in: :body, schema: {
+          type: :object,
+          properties: {
+            user: {
+              type: :object,
+              properties: {
+                email: { type: :string },
+              },
+              required: %w[email]
+            }
+          },
+          required: %w[user]
+        }
+        
+  
+        response '200', 'Conformation send successfully please check your email' do
+          schema type: :object,
+                 properties: {
+                   status: {
+                     type: :object,
+                     properties: {
+                       code: { type: :integer },
+                       message: { type: :string },
+
+                      },
+                      required: %w[code message]
+                    },
+                      },
+                 required: %w[status]
+  
+          run_test! do
+        
+            post '/users/confirmation', params: { user: { email: 'test@example.com' } }
+  
+  
             expect(response).to have_http_status(:ok)
   
-            # Assert the response body against the defined schema
+            json_response = JSON.parse(response.body)
+            expect(json_response).to include('status')
+            expect(json_response['status']).to include('code', 'message')
+          end
+        end
+  
+        response '401', "Couldn't find an active session" do
+          schema type: :object,
+                 properties: {
+                   status: { type: :integer },
+                   message: { type: :string }
+                 },
+                 required: %w[status message]
+  
+          run_test! do
+            post '/users/confirmation', params: { user: { email: 'invalid@example.com' } }
+  
+            expect(response).to have_http_status(:unauthorized)
             json_response = JSON.parse(response.body)
             expect(json_response).to include('status', 'message')
           end
