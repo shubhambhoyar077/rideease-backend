8255490dbef83ff07950ca95d33b196437f12747
shubham <shubham.bhoyar077@gmail.com>
2023-07-20 23:22:45 +0530

    Add test and api for forget and confirmation  mail

--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -1,8 +1,10 @@
 require 'swagger_helper'
+require 'devise/jwt/test_helpers'
 
 RSpec.describe Users::SessionsController, type: :request do
   before do
     @user = User.create(email: 'test100@example.com', password: '123456')
+    @user1 = User.create(email: 'test111@example.com', password: '123456')
     @user.confirm
   end
   path '/users' do
@@ -97,169 +99,116 @@ RSpec.describe Users::SessionsController, type: :request do
     end
   end
 
-  #   path '/users/sign_out' do
-  #     delete 'User Sign Out' do
-  #       tags 'Users'
-  #       produces 'application/json'
-  #       security [bearer_auth: []]
+    path '/users/sign_out' do
+      delete 'User Sign Out' do
+        tags 'Users'
+        produces 'application/json'
+        security [bearer_auth: []]
   
-  #       response '200', 'Logged out successfully' do
-  #         schema type: :object,
-  #                properties: {
-  #                  status: { type: :integer },
-  #                  message: { type: :string }
-  #                },
-  #                required: %w[status message]
-  
-  #         run_test! do
-  #           @user = User.create!(full_name: 'John Doe', email: 'john@example.com', password: 'password', role: 'user')
-  #           @user.config
-  
-  #           post '/users/sign_in', params: { user: { email: 'john@example.com', password: 'password' } }
-  #           auth_token = response.headers['Authorization']
-  #           delete '/users/sign_out', headers: { 'Authorization' => auth_token }
-  
-  #           expect(response).to have_http_status(:ok)
-  #           json_response = JSON.parse(response.body)
-  #           expect(json_response).to include('status', 'message')
-  #         end
-  #       end
-  #     end
-  #   end
+        response '200', 'Logged out successfully' do
+          include Devise::JWT::TestHelpers
 
-  #   path '/users/confirmation' do
-  #     post 'Send conformation email again' do
-  #       tags 'Users'
-  #       consumes 'application/json'
-  #       produces 'application/json'
-  #       parameter name: :user, in: :body, schema: {
-  #         type: :object,
-  #         properties: {
-  #           user: {
-  #             type: :object,
-  #             properties: {
-  #               email: { type: :string },
-  #             },
-  #             required: %w[email]
-  #           }
-  #         },
-  #         required: %w[user]
-  #       }
+          before do
+            sign_in @user
+          end
+    
+          let(:jwt_token) { Warden::JWTAuth::UserEncoder.new.call(@user, :user, nil).first }
+      
+      # Set the Authorization header with the JWT token
+      let(:Authorization) { "Bearer #{jwt_token}" }
+          run_test!
+     
+        end
+      end
+    end
+
+    path '/users/confirmation' do
+      post 'Send conformation email again' do
+        tags 'Users'
+        consumes 'application/json'
+        produces 'application/json'
+        parameter name: :user, in: :body, schema: {
+          type: :object,
+          properties: {
+            user: {
+              type: :object,
+              properties: {
+                email: { type: :string },
+              },
+              required: %w[email]
+            }
+          },
+          required: %w[user]
+        }
         
   
-  #       response '200', 'Conformation send successfully please check your email' do
-  #         schema type: :object,
-  #                properties: {
-  #                  status: {
-  #                    type: :object,
-  #                    properties: {
-  #                      code: { type: :integer },
-  #                      message: { type: :string },
+        response '200', 'Conformation send successfully please check your email' do
+          let(:user) { { "user": { "email": "test111@example.com" } } }
+  
+          run_test! do |response|
+            expect(response).to have_http_status(:ok)
+  
+            json_response = JSON.parse(response.body)
+            expect(json_response).to include('status')
+            expect(json_response['status']).to include('code', 'message')
+          end
+        end
+  
+        response '200', "User does not exists" do
+          let(:user) { { "user": { "email": "t@example.com" } } }
+  
+          run_test! do |response|
+            json_response = JSON.parse(response.body)
+            expect(json_response['status']['message']).to include('User does not exist')
+          end
+        end
+      end
+    end
 
-  #                     },
-  #                     required: %w[code message]
-  #                   },
-  #                     },
-  #                required: %w[status]
-  
-  #         run_test! do
-        
-  #           post '/users/confirmation', params: { user: { email: 'test@example.com' } }
-  
-  
-  #           expect(response).to have_http_status(:ok)
-  
-  #           json_response = JSON.parse(response.body)
-  #           expect(json_response).to include('status')
-  #           expect(json_response['status']).to include('code', 'message')
-  #         end
-  #       end
-  
-  #       response '401', "Couldn't find an active session" do
-  #         schema type: :object,
-  #                properties: {
-  #                  status: { type: :integer },
-  #                  message: { type: :string }
-  #                },
-  #                required: %w[status message]
-  
-  #         run_test! do
-  #           post '/users/confirmation', params: { user: { email: 'invalid@example.com' } }
-  
-  #           expect(response).to have_http_status(:unauthorized)
-  #           json_response = JSON.parse(response.body)
-  #           expect(json_response).to include('status', 'message')
-  #         end
-  #       end
-  #     end
-  #   end
-
-  #   path '/users/password' do
-  #     post 'Send forget password email' do
-  #       tags 'Users'
-  #       consumes 'application/json'
-  #       produces 'application/json'
-  #       parameter name: :user, in: :body, schema: {
-  #         type: :object,
-  #         properties: {
-  #           user: {
-  #             type: :object,
-  #             properties: {
-  #               email: { type: :string },
-  #             },
-  #             required: %w[email]
-  #           }
-  #         },
-  #         required: %w[user]
-  #       }
+    path '/users/password' do
+      post 'Send forget password email' do
+        tags 'Users'
+        consumes 'application/json'
+        produces 'application/json'
+        parameter name: :user, in: :body, schema: {
+          type: :object,
+          properties: {
+            user: {
+              type: :object,
+              properties: {
+                email: { type: :string },
+              },
+              required: %w[email]
+            }
+          },
+          required: %w[user]
+        }
         
   
-  #       response '200', 'Forget password email send successfully' do
-  #         schema type: :object,
-  #                properties: {
-  #                  status: {
-  #                    type: :object,
-  #                    properties: {
-  #                      code: { type: :integer },
-  #                      message: { type: :string },
-
-  #                     },
-  #                     required: %w[code message]
-  #                   },
-  #                     },
-  #                required: %w[status]
+        response '200', 'Forget password email send successfully' do
+          let(:user) { { "user": { "email": "test100@example." } } }
   
-  #         run_test! do
-        
-  #           post '/users/password', params: { user: { email: 'test@example.com' } }
+          run_test! do |response|
+  
+            expect(response).to have_http_status(:ok)
+  
+            json_response = JSON.parse(response.body)
+            expect(json_response).to include('status')
+            expect(json_response['status']).to include('code', 'message')
+          end
+        end
+  
+        response '200', "User does not exists" do
+          let(:user) { { "user": { "email": "tes@example.com" } } }
   
   
-  #           expect(response).to have_http_status(:ok)
-  
-  #           json_response = JSON.parse(response.body)
-  #           expect(json_response).to include('status')
-  #           expect(json_response['status']).to include('code', 'message')
-  #         end
-  #       end
-  
-  #       response '401', "Couldn't find an active session" do
-  #         schema type: :object,
-  #                properties: {
-  #                  status: { type: :integer },
-  #                  message: { type: :string }
-  #                },
-  #                required: %w[status message]
-  
-  #         run_test! do
-  #           post '/users/password', params: { user: { email: 'invalid@example.com' } }
-  
-  #           expect(response).to have_http_status(:unauthorized)
-  #           json_response = JSON.parse(response.body)
-  #           expect(json_response).to include('status', 'message')
-  #         end
-  #       end
-  #     end
-  #   end
+          run_test! do |response|
+            json_response = JSON.parse(response.body)
+            expect(json_response['status']['message']).to include('User does not exist')
+          end
+        end
+      end
+    end
   
   
 end
