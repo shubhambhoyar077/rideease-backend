bfabb98ee9eddb4ee1e85be66dbe855fde5c8adc
shubham <shubham.bhoyar077@gmail.com>
2023-07-20 13:24:10 +0530

    Add swagger for api doc

--- spec/requests/api/services_spec.rb
+++ spec/requests/api/services_spec.rb
@@ -2,6 +2,38 @@ require 'rails_helper'
 
 RSpec.describe 'Api::Services', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+
+    path '/api/services' do
+
+      get 'Retrieves a cars' do
+        tags 'Cars'
+        produces 'application/json', 'application/xml'
+
+        # request_body_example value: { some_field: 'Foo' }, name: 'basic', summary: 'Request example description'
+  
+        response '200', 'cars list' do
+          schema type: :object,
+            properties: {
+              id: { type: :integer },
+              name: { type: :string },
+              image: { type: :string }
+            },
+            required: [ 'id', 'name', 'image' ]
+  
+          let(:id) { Service.create(name: 'foo', image: 'bar.jpg').id }
+          run_test!
+        end
+  
+        response '404', 'blog not found' do
+          let(:id) { 'invalid' }
+          run_test!
+        end
+  
+        response '406', 'unsupported accept header' do
+          let(:'Accept') { 'application/foo' }
+          run_test!
+        end
+      end
+    end
   end
 end
