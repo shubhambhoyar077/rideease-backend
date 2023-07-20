7fc3801ded1f309405cd562255e12caf7f633181
Ismail <ismailmunyentwari9@gmail.com>
2023-07-20 12:49:38 +0300

    Add admin abilities

--- db/migrate/20230712141929_create_users.rb
+++ db/migrate/20230712141929_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :name, null: false, default: ""
       t.string :email, null: false, default: ""
+      t.string :role, null: false, default: ""
 
       t.timestamps
     end
