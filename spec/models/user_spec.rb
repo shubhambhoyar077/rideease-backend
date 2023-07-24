4ad27c7b88eeb045c24c65bfcfb150bb55e23ec8
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-07-24 16:21:11 +0300

    Added Unit testing for User Model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    User.new(name: 'John Doe', email: 'john@gmail.com', password: '11111111')
+  end
+  before { subject.save }
+
+  it 'email should not be empty' do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'name should be implemented' do
+    expect(subject.name).to eql 'John Doe'
+  end
+
+  it 'email should be implemented' do
+    expect(subject.email).to eql 'john@gmail.com'
+  end
 end
